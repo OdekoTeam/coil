38d87d223800c9ad88a19447766bd8ef3dd218a9
ivanbrennan <ivan.brennan@gmail.com>
2024-05-30 21:16:09 -0400

    adjust type annotations
    Adjust the structure of type annotations to align with the annotations
    that are generated when integrating this engine into an existing
    project. The namespacing, in particular, was causing errors when trying
    to typecheck in a downstream app.

--- spec/dummy/app/lib/type_aliases.rb
+++ spec/dummy/app/lib/type_aliases.rb
@@ -1,5 +1,8 @@
 # typed: strict
 
+# The type aliases declared in rbi/ohm.rbi can in fact be referenced from an app
+# that uses Ohm. However, this does not seem to be the case for test Dummy app,
+# so we work around the issue by mirroring those aliases here.
 module TypeAliases
   AnyMessage = T.type_alias {
     T.any(
