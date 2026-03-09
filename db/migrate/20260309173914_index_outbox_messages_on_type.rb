3fb0d45977f8a3c715aa874e6522325513c88f73
ivanbrennan <ivan.brennan@gmail.com>
2026-03-09 13:42:45 -0400

    index inbox/outbox messages on `type`
    Indexing on `type` improves performance when a periodic job queries for
    all distinct message types, e.g.
    
        SELECT DISTINCT "type" FROM "coil_inbox_messages"
    
    The existing indices on `(type, key)` involve significantly more data,
    because `key` is a jsonb column. This can (and often does) cause the
    query planner to choose a full table scan over an index scan. The
    decision may be influenced by whether database maintenance tasks (e.g.
    autovaccuming) have run recently, and how up-to-date the database
    statistics are.
    
    A simple index on `type` provides a more efficient option, making it far
    less likely that the query planner will choose a full table scan.

---
+++ db/migrate/20260309173914_index_outbox_messages_on_type.rb
@@ -0,0 +1,23 @@
+# typed: false
+
+class IndexOutboxMessagesOnType < ActiveRecord::Migration[8.1]
+  disable_ddl_transaction!
+
+  # Indexing on `type` improves performance when a periodic job queries for all
+  # distinct message types, i.e.
+  #
+  #   SELECT DISTINCT "type" FROM "coil_outbox_messages"
+  #
+  # The existing indices on `(type, key)` involve significantly more data,
+  # because `key` is a jsonb column. This can (and often does) cause the query
+  # planner to choose a full table scan over an index scan. The decision may
+  # depend on whether database maintenance tasks (e.g. autovaccuming) have run
+  # recently, and how up-to-date the database statistics are.
+  #
+  # A simple index on `type` provides a more efficient option, making it far
+  # less likely that the query planner will choose a full table scan.
+
+  def change
+    add_index :coil_outbox_messages, :type, algorithm: :concurrently
+  end
+end
